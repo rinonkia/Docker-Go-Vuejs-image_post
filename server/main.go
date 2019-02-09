0ffcbaaed004b7cbcce55aaad62203b94afa715a
gatapon <git@gatapon.net>
2019-02-10 01:40:36 +0900

    method create

--- server/main.go
+++ server/main.go
@@ -1,13 +1,22 @@
 package main // import "server"
 
-import "github.com/gin-gonic/gin"
+import (
+	"server/handler"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/cors"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping",
-		})
-	})
+
+	r.Use(cors.New(cors.Config{
+		AllwoOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"*"},
+	}))
+
+	r.POST("/images", handler.Upload)
 	r.Run(":8888")
 }
